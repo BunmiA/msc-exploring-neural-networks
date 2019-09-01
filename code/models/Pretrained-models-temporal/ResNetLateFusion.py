ee44420074acd9760f338189b8f25875a5905cca
olubunmiaworanti <olubunmi.aworanti@gmail.com>
2019-09-01 19:45:30 +0100

    adding the initial pretrained python scripts model

---
+++ code/models/Pretrained-models-temporal/ResNetLateFusion.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import myCallbacks as c
+
+# import tensorflow_datasets as tfds
+
+callbacks = c.highAccCallback()
+base_model = tf.keras.applications.resnet.ResNet50(input_shape=IMG_SHAPE,
+                                               include_top=False,
+                                               weights='imagenet')
+
+base_model.trainable = False
+base_model.summary()
+
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+feature_batch_average = global_average_layer(feature_batch)
+print(feature_batch_average.shape)
+
+prediction_layer = tf.keras.layers.Dense(101, activation='softmax')
+prediction_batch = prediction_layer(feature_batch_average)
+print(prediction_batch.shape)
+
+model = tf.keras.Sequential([
+    base_model,
+    tf.keras.layers.Dropout(0.5),
+    global_average_layer,
+    prediction_layer
+])
+
+base_learning_rate = 0.0001
+model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=base_learning_rate),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+model.fit(train_batches,
+          epochs=2,verbose=1,
+          callbacks=[callbacks] )
+#                     validation_da
